aabcc0a5e6012e4c6686bd6f9593693f09f01d9b
Alex Gibson <alexgibson@users.noreply.github.com>
2024-06-28 14:02:07 +0100

    Add optional check for experiment cookie to persist previously seen variations (Fixes #93)

--- demo/src/public/experiment-page3-redirect.js
+++ demo/src/public/experiment-page3-redirect.js
@@ -1,7 +1,25 @@
 (function () {
     'use strict';
 
+    function setVariationCookie(exp) {
+        // set cookie to expire in 24 hours
+        var date = new Date();
+        date.setTime(date.getTime() + 1 * 24 * 60 * 60 * 1000);
+        var expires = date.toUTCString();
+
+        window.Mozilla.Cookies.setItem(
+            exp.id,
+            exp.chosenVariation,
+            expires,
+            undefined,
+            undefined,
+            false,
+            'lax'
+        );
+    }
+
     var lou = new window.TrafficCop({
+        id: 'my-experiment-id-4',
         variations: {
             'v=a': 40,
             'v=b': 40
@@ -9,4 +27,6 @@
     });
 
     lou.init();
+
+    setVariationCookie(lou);
 })();
